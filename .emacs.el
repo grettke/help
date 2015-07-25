142b8a70ab601253542c910774b19eb66d1d116d
Grant Rettke <gcr@wisdomandwonder.com>
2015-07-25 14:42:32 -0500

    Initialize Package with MELPA and GNU.

--- .emacs.el
+++ .emacs.el
@@ -3,5 +3,10 @@
 (add-to-list 'load-path "~/src/org-mode/lisp")
 (add-to-list 'load-path "~/src/org-mode/contrib/lisp")
 (require 'org)
+(package-initialize)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'load-path "~/src/use-package")
 (require 'use-package)
