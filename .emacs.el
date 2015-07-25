14b662ef5c9e50d5f8bd98121bbdb9c7348296ed
Grant Rettke <gcr@wisdomandwonder.com>
2015-07-25 14:24:36 -0500

    Configure use-package.

--- .emacs.el
+++ .emacs.el
@@ -3,3 +3,5 @@
 (add-to-list 'load-path "~/src/org-mode/lisp")
 (add-to-list 'load-path "~/src/org-mode/contrib/lisp")
 (require 'org)
+(add-to-list 'load-path "~/src/use-package")
+(require 'use-package)
