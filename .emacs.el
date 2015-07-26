8203f1e74e0785f8793981d24292b73ae5d5467b
Grant Rettke <gcr@wisdomandwonder.com>
2015-07-25 19:20:08 -0500

    Line numbers make things pleasant.

--- .emacs.el
+++ .emacs.el
@@ -59,6 +59,8 @@ Attribution: URL `http://orgmode.org/manual/System_002dwide-header-arguments.htm
      (apply orig-fun args)))
  (advice-add 'yes-or-no-p :around #'help/yes-or-no-p)
  (advice-add 'y-or-n-p :around #'help/yes-or-no-p))
+(add-hook #'text-mode-hook #'linum-mode)
+(add-hook #'prog-mode-hook #'linum-mode)
 (use-package solarized-theme
   :ensure t
   :config
