3971f9b93c886e40447bdf7b06ee971d918e56c3
Grant Rettke <gcr@wisdomandwonder.com>
2015-08-07 20:08:31 -0500

    Always load the color theme.

--- .emacs.el
+++ .emacs.el
@@ -1075,7 +1075,7 @@ Attribution: URL `https://lists.gnu.org/archive/html/emacs-orgmode/2015-01/msg00
   (setq solarized-use-less-bold t)
   (setq solarized-use-more-italic nil)
   (setq solarized-emphasize-indicators nil)
-  (load-theme 'solarized-dark))
+  (load-theme 'solarized-dark t))
 (setq comint-scroll-to-bottom-on-input 'this)
 (setq comint-scroll-to-bottom-on-output 'others)
 (setq comint-move-point-for-output 'others)
@@ -1161,17 +1161,3 @@ Attribution: URL `https://lists.gnu.org/archive/html/emacs-orgmode/2015-01/msg00
 (global-set-key (kbd "C-n") #'next-logical-line)
 (global-set-key (kbd "s-C-p") #'previous-line)
 (global-set-key (kbd "C-p") #'previous-logical-line)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
