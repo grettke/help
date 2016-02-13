63b99141fba33838bec40b232a7b6cbe3db99b25
Grant Rettke <gcr@wisdomandwonder.com>
2016-02-13 14:49:56 -0600

    Refactored out the ECM config.
    Poor doc design resulted in 6m tangle.
    
    Down to 4.5m now.

--- .org-mode-fundamentals.emacs.el
+++ .org-mode-fundamentals.emacs.el
@@ -1,26 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
 ;; [[file:help.org::*Org-Mode%20Fundamentals][78D777D3-FA69-4361-874C-7262DE96F753]]
-;; [[file:~/src/help/help.org::*Org-Mode%20Exemple%20Complet%20Minimal][9969960B-FFEE-48B4-BD9C-F29E750FD8A3]]
-;; [[file:~/src/help/help.org::*Load%20Behavior][87B0CAB9-E6E5-4C68-B389-9289E65FCA7F]]
-(setq load-prefer-newer t)
-;; 87B0CAB9-E6E5-4C68-B389-9289E65FCA7F ends here
-;; [[file:~/src/help/help.org::*Org-Mode%20Exemple%20Complet%20Minimal][D5D4EECF-00C5-4B2F-B9F9-988591C70852]]
-(add-to-list 'load-path "~/src/org-mode/lisp")
-;; D5D4EECF-00C5-4B2F-B9F9-988591C70852 ends here
-;; [[file:~/src/help/help.org::*Org-Mode%20Exemple%20Complet%20Minimal][4D4E921E-7B99-43B2-AD1F-30D2593F05D7]]
-(add-to-list 'load-path "~/src/org-mode/contrib/lisp")
-;; 4D4E921E-7B99-43B2-AD1F-30D2593F05D7 ends here
-;; [[file:~/src/help/help.org::*Org-Mode%20Exemple%20Complet%20Minimal][28776245-A6BB-4294-AC72-30F03F902DF7]]
-(setq org-list-allow-alphabetical t)
-;; 28776245-A6BB-4294-AC72-30F03F902DF7 ends here
-;; [[file:~/src/help/help.org::*Org-Mode%20Exemple%20Complet%20Minimal][5112469C-F95A-484B-9D56-90DDC863686A]]
-(setq org-enforce-todo-checkbox-dependencies t)
-;; 5112469C-F95A-484B-9D56-90DDC863686A ends here
-;; [[file:~/src/help/help.org::*Org-Mode%20Exemple%20Complet%20Minimal][86CAEC4F-CFC4-4CAD-9993-E1DCEF3BFCD1]]
-(require 'org)
-;; 86CAEC4F-CFC4-4CAD-9993-E1DCEF3BFCD1 ends here
-;; 9969960B-FFEE-48B4-BD9C-F29E750FD8A3 ends here
+(load-file "~/src/help/.org-mode-ecm.emacs.el")
+
 ;; [[file:~/src/help/help.org::*Helper%20Functions][BB2E97AF-6364-401F-8063-8B5A0BE481E6]]
 (defun help/set-org-babel-default-header-args (property value)
   "Easily set system header arguments in org mode.
