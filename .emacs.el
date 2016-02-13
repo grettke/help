133c0e83043356372f9c1d965a8baf56e0e1f160
Grant Rettke <gcr@wisdomandwonder.com>
2016-02-13 15:43:33 -0600

    Factored out Org2Blog.
    Tangling down from 4.5m to 2.5m

--- .emacs.el
+++ .emacs.el
@@ -1,48 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 ;; [[file:help.org::*The%20Whole%20Enchilada][AD12BE48-B87B-4AB6-814D-4FA5E47597A0]]
-;; [[file:~/src/help/help.org::*Org2blog][E32697AE-784E-458F-A042-D9B559908721]]
-(load-file "~/src/help/.org-mode-fundamentals.emacs.el")
-;; [[file:~/src/help/help.org::*Watch%20What%20You%20Eat][804FD197-8B2A-49F5-A4CB-48F6036FBCEC]]
-(package-initialize)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("gnu" . "http://elpa.gnu.org/packages/") t)
-;; 804FD197-8B2A-49F5-A4CB-48F6036FBCEC ends here
-;; [[file:~/src/help/help.org::*Watch%20What%20You%20Eat][FAB22F99-AB0C-4A75-AFEF-9E680A5EE95D]]
-(add-to-list 'load-path"~/src/use-package")
-;; FAB22F99-AB0C-4A75-AFEF-9E680A5EE95D ends here
-;; [[file:~/src/help/help.org::*Watch%20What%20You%20Eat][54841CA9-6491-46E8-8F40-4DCF7182CCD7]]
-(eval-when-compile
-  (require 'use-package))
-(use-package diminish)
-;; 54841CA9-6491-46E8-8F40-4DCF7182CCD7 ends here
-;; [[file:~/src/help/help.org::*Blog][E99598C8-D394-484B-A7FB-333FA2544CB0]]
-(use-package xml-rpc
-  :ensure t)
-;; E99598C8-D394-484B-A7FB-333FA2544CB0 ends here
-;; [[file:~/src/help/help.org::*Blog][79F63BAD-0CD7-4A97-8A7D-CBAA51BB48E1]]
-(use-package metaweblog
-  :ensure t)
-;; 79F63BAD-0CD7-4A97-8A7D-CBAA51BB48E1 ends here
-;; [[file:~/src/help/help.org::*Blog][DF522DDF-4A71-4D8B-B6F9-9BD6F11EB832]]
-(add-to-list 'load-path "~/src/org2blog")
-(require 'org2blog-autoloads)
-;; DF522DDF-4A71-4D8B-B6F9-9BD6F11EB832 ends here
-;; [[file:~/src/help/help.org::*Blog][9C1EAE6B-9D05-43FB-AC35-7FBFE6CB4F7E]]
-(setq org2blog/wp-track-posts nil)
-;; 9C1EAE6B-9D05-43FB-AC35-7FBFE6CB4F7E ends here
-;; [[file:~/src/help/help.org::*Blog][BCA2FE29-930C-4E66-A662-17FF451D1591]]
-(setq org2blog/wp-blog-alist
-      '(("wisdomandwonder"
-         :url "https://www.wisdomandwonder.com/xmlrpc.php"
-         :username "admin"
-         :default-categories ("Article" "Link")
-         :confirm t
-         :show 'show)))
-;; BCA2FE29-930C-4E66-A662-17FF451D1591 ends here
-;; E32697AE-784E-458F-A042-D9B559908721 ends here
+(load-file "~/src/help/.org-mode-org2blog.emacs.el")
 ;; [[file:~/src/help/help.org::*Display][20CC11BB-D72C-4A86-8558-44D9AE44FEAF]]
 (defmacro help/on-gui (statement &rest statements)
   "Evaluate the enclosed body only when run on GUI."
