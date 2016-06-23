2b493a8a41cd44e44132f5432f37d7b7840c4378
Grant Rettke <gcr@wisdomandwonder.com>
2016-06-22 19:50:19 -0500

    Use APL Mode from ELPA

--- .emacs.el
+++ .emacs.el
@@ -1837,8 +1837,8 @@ _c_ org-fill-para _b_ swtch2sessn _n_ n2sbtre"
 ;; [[file:help.org::845637D6-CAEF-43FF-AFAB-166EC0E21FC4][845637D6-CAEF-43FF-AFAB-166EC0E21FC4]]
 (setq gnu-apl-mode-map-prefix "H-")
 (setq gnu-apl-interactive-mode-map-prefix "H-")
-(add-to-list 'load-path "~/git/github/gnu-apl-mode")
-(require 'gnu-apl-mode)
+(use-package gnu-apl-mode
+  :ensure t)
 ;; 845637D6-CAEF-43FF-AFAB-166EC0E21FC4 ends here
 
 ;; [[file:help.org::E7AE32C1-42CC-482F-92D2-43DBD703500C][E7AE32C1-42CC-482F-92D2-43DBD703500C]]
