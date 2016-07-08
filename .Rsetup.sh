798a6b501d7c57a058c9d310d60f8e1e8c64c237
Grant Rettke <gcr@wisdomandwonder.com>
2016-07-07 19:52:24 -0500

    Another new build

--- .Rsetup.sh
+++ .Rsetup.sh
@@ -1,1 +1,5 @@
-/Users/gcr/.Rsetup.sh
+rm -rf ~/.Rpackages
+mkdir ~/.Rpackages
+ln -sfn ~/.Rprofile ~/src/help/.Rprofile
+ln -sfn ~/.Renviron ~/src/help/.Renviron
+ln -sfn ~/.Rinstall.r ~/src/help/.Rinstall.r
