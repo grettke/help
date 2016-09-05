86a76f7e1e47a0ac32e9f0990b21eebfa6c35243
Grant Rettke <gcr@wisdomandwonder.com>
2016-09-05 09:46:01 -0500

    Corrected f10 to f8

--- .emacs.el
+++ .emacs.el
@@ -2613,6 +2613,7 @@ Attribution: URL `http://permalink.gmane.org/gmane.emacs.orgmode/98153'.")
 
 ;; [[file:help.org::orgmode:gcr:vela:8D9EC2AC-FCF0-4A64-9C84-7EF272BC802F][orgmode:gcr:vela:8D9EC2AC-FCF0-4A64-9C84-7EF272BC802F]]
 (global-set-key (kbd "<f9>") #'org2blog/wp-new-entry)
+(global-set-key (kbd "<f8>") #'org2blog/wp-post-buffer-and-publish)
 ;; orgmode:gcr:vela:8D9EC2AC-FCF0-4A64-9C84-7EF272BC802F ends here
 
 ;; [[file:help.org::orgmode:gcr:vela:E4B98196-FDFB-42B4-A52A-8CA7DC066E8E][orgmode:gcr:vela:E4B98196-FDFB-42B4-A52A-8CA7DC066E8E]]
