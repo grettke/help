053f8bd28351e5b1f12273871290ef3b5e850510
Grant Rettke <gcr@wisdomandwonder.com>
2017-02-28 19:16:55 -0600

    Easier to track focus changes (of flow)

--- .emacs.el
+++ .emacs.el
@@ -2883,7 +2883,7 @@ Attribution: URL `https://www.emacswiki.org/emacs/FindingNonAsciiCharacters'"
   :ensure t
   :config
   (setq solarized-distinct-fringe-background t)
-  (setq solarized-high-contrast-mode-line nil)
+  (setq solarized-high-contrast-mode-line t)
   (setq solarized-use-less-bold t)
   (setq solarized-use-more-italic nil)
   (setq solarized-emphasize-indicators nil)
