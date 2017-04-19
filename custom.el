45d4b646e13be7c960d9088f375657e6f4f04f2d
Grant Rettke <gcr@wisdomandwonder.com>
2017-04-19 13:20:54 -0500

    Re-installed from scratch.

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ssh-config-mode writegood-mode wrap-region web-mode unicode-troll-stopper unicode-escape undo-tree stripe-buffer solarized-theme smex smartparens selectric-mode scad-mode rainbow-mode racket-mode r-autoyas projectile pretty-mode plantuml-mode persistent-soft page-break-lines ox-gfm org-table-sticky-header nlinum mwim metaweblog markdown-mode magit macrostep lexbind-mode langtool key-chord json-mode js2-refactor inlineR imenu-list ido-vertical-mode ido-ubiquitous ido-hacks hydra graphviz-dot-mode gnu-apl-mode gitignore-mode gitconfig-mode gitattributes-mode git-timemachine fuzzy fraktur-mode flycheck-package flx-ido fill-column-indicator expand-region exec-path-from-shell eval-in-repl ess-R-object-popup ess-R-data-view elmacro elisp-slime-nav elisp-refs dired-imenu dired-details+ diminish diff-hl dash-functional dash-at-point bug-hunter browse-kill-ring blackboard-bold-mode auto-complete-chunk auctex apache-mode anzu alert aggressive-indent ag ac-geiser))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
