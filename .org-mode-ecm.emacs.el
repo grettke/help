3d8bdf5355e85d50287eef389eb035bec3760ed8
Grant Rettke <gcr@wisdomandwonder.com>
2017-05-11 14:05:02 -0500

    Never use unicode delimeters:
    - Makes it painful for others to use
    - Forces unicode Org-Mode buffer

--- .org-mode-ecm.emacs.el
+++ .org-mode-ecm.emacs.el
@@ -4,6 +4,4 @@
 (add-to-list 'load-path "~/src/org-mode/contrib/lisp")
 (setq org-list-allow-alphabetical t)
 (setq org-enforce-todo-checkbox-dependencies t)
-(setq org-babel-noweb-wrap-start "«")
-(setq org-babel-noweb-wrap-end "»")
 (require 'org)
