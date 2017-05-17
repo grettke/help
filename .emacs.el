4aece8e21479e0e2d6cb0af86a98c28e19c6ebe2
Grant Rettke <gcr@wisdomandwonder.com>
2017-05-17 15:47:07 -0500

    VV-Hydra->Grep

--- .emacs.el
+++ .emacs.el
@@ -3232,7 +3232,7 @@ ALPHA : [ %(frame-parameter nil 'alpha) ].'
 _O_ base64-encode-region _P_ base64-decode-region _|_ split-window-horizontally _-_ split-window-vertically
 _1_ reset-font _2_ -font  _3_ +font _4_ ellipsis _5_ UUID _6_ bfr-cdng-systm _7_ flyck/buf _&_ flyck/toggle _*_ flyck/list _8_ grade-level _9_ reading-ease _0_ writegood _w_ widen _=_ reposition-window _t_ rectangle-mark _y_ transparency _u_ ucs-insert  _i_ scrollUp _I_ prevLogLine _o_ dbgOnErr _p_ query-replace _[_ ↑page _]_ ↓page _}_ transliterate
 _T_ trademarks
-_a_ ag  _A_ apropo'ish _s_ help/toggle-mac-right-option-modifier _S_ help/toggle-mac-function-modifier _d_ dash-at-point  _D_ detangle _j_ obtj2o _k_ scrollDown _K_ nextLogLine  _;_ toggle-lax-whitespace
+_a_ ag  _A_ apropo'ish _s_ help/toggle-mac-right-option-modifier _S_ help/toggle-mac-function-modifier _d_ dash-at-point  _D_ detangle _g_ grep _j_ obtj2o _k_ scrollDown _K_ nextLogLine  _;_ toggle-lax-whitespace
 _l_ visual-line-mode _L_ aggressive-indent-mode
 _x_ delete-indentation _c_ fill-paragraph _v_ desc-symbol _V_ view-mode _b_ erase-buffer _B_ibtex _n_ normal _m_ desc/mode _M_ checks
 _<_ cmtIn _>_ cmtOut _?_ snp"
@@ -3257,6 +3257,7 @@ _<_ cmtIn _>_ cmtOut _?_ snp"
   ("a" ag)
   ("A" hydra-apropos-ish/body)
   ("s" help/toggle-mac-right-option-modifier)
+  ("g" grep)
   ("h" hack-local-variable)
   ("l" visual-line-mode)
   ("L" aggressive-indent-mode)
