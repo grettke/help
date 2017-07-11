a9ab5c66dc8c5df1c5bff4cdbedbb770747f2157
Grant Rettke <gcr@wisdomandwonder.com>
2017-07-10 22:58:33 -0500

    Eyebrowse hydra.

--- .emacs.el
+++ .emacs.el
@@ -3386,7 +3386,30 @@ ALPHA : [ %(frame-parameter nil 'alpha) ].'
   :ensure t
   :config
   (setq eyebrowse-wrap-around t)
-  (eyebrowse-mode t))
+  (eyebrowse-mode t)
+  (defhydra help/hydra-left-side/eyebrowse (:color blue :hint nil)
+    "
+current eyebrowse slot: %(eyebrowse--get 'current-slot)
+ _j_ previous _k_ last _l_ next _u_ close _i_ choose _o_ rename _q_ quit
+   _a_ 00 _s_ 01 _d_ 02 _f_ 03 _g_ 04 _z_ 05 _x_ 06 _c_ 07 _v_ 08 _b_ 09"
+    ("j" #'eyebrowse-prev-window-config :exit nil)
+    ("k" #'eyebrowse-last-window-config)
+    ("l" #'eyebrowse-next-window-config :exit nil)
+    ("u" #'eyebrowse-close-window-config :exit nil)
+    ("i" #'eyebrowse-switch-to-window-config)
+    ("o" #'eyebrowse-rename-window-config :exit nil)
+    ("q" nil)
+    ("a" #'eyebrowse-switch-to-window-config-0)
+    ("s" #'eyebrowse-switch-to-window-config-1)
+    ("d" #'eyebrowse-switch-to-window-config-2)
+    ("f" #'eyebrowse-switch-to-window-config-3)
+    ("g" #'eyebrowse-switch-to-window-config-4)
+    ("z" #'eyebrowse-switch-to-window-config-5)
+    ("x" #'eyebrowse-switch-to-window-config-6)
+    ("c" #'eyebrowse-switch-to-window-config-7)
+    ("v" #'eyebrowse-switch-to-window-config-8)
+    ("b" #'eyebrowse-switch-to-window-config-9))
+  (global-set-key (kbd "C-M-e") #'help/hydra-left-side/eyebrowse/body))
 ;; org_gcr_2017-07-08_mara_3D694F1B-EB4A-4724-A8E7-61F09C4773A5 ends here
 
 ;; [[file:~/src/help/help.org::org_gcr_2017-05-12_mara_8125C96A-8971-45FC-A8D2-30FDC438B71C][org_gcr_2017-05-12_mara_8125C96A-8971-45FC-A8D2-30FDC438B71C]]
