2b85d3e003249569054bf698f5bc3e6a6da8fc52
Grant Rettke <gcr@wisdomandwonder.com>
2017-07-13 15:38:33 -0500

    Always switching to it

--- .emacs.el
+++ .emacs.el
@@ -3454,6 +3454,8 @@ current eyebrowse slot: %(eyebrowse--get 'current-slot)
 ;; org_gcr_2017-07-08_mara_3D694F1B-EB4A-4724-A8E7-61F09C4773A5 ends here
 
 ;; [[file:~/src/help/help.org::org_gcr_2017-05-12_mara_8125C96A-8971-45FC-A8D2-30FDC438B71C][org_gcr_2017-05-12_mara_8125C96A-8971-45FC-A8D2-30FDC438B71C]]
+(global-set-key (kbd "C-8") (lambda () (interactive) (switch-to-buffer
+                                                      "*scratch*")))
 (global-set-key (kbd "C-9") (lambda () (interactive) (switch-to-buffer
                                                       "projects.org")))
 (global-set-key (kbd "C-0") (lambda () (interactive) (switch-to-buffer
