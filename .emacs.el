62b3f353fd2b7a79a8750f7e2d19095c91a7cc4e
Grant Rettke <gcr@wisdomandwonder.com>
2017-07-25 21:10:42 -0500

    Better eyebrowse hydra

--- .emacs.el
+++ .emacs.el
@@ -3637,14 +3637,13 @@ this frame's opacity: %(frame-parameter nil 'alpha)
   (defhydra help/hydra-left-side/eyebrowse (:color blue :hint nil)
     "
 current eyebrowse slot: %(eyebrowse--get 'current-slot)
- _j_ previous _k_ last _l_ next _u_ close _i_ choose _o_ rename _q_ quit
-   _a_ 00 _s_ 01 _d_ 02 _f_ 03 _g_ 04 _z_ 05 _x_ 06 _c_ 07 _v_ 08 _b_ 09"
+ _j_ previous _k_ next _i_ choose _l_ last _n_ rename _q_ quit
+   _a_ 00 _s_ 01 _d_ 02 _f_ 03 _g_ 04 _z_ 05 _x_ 06 _c_ 07 _v_ 08 _b_ 09 _~_ close "
     ("j" #'eyebrowse-prev-window-config :exit nil)
-    ("k" #'eyebrowse-last-window-config)
-    ("l" #'eyebrowse-next-window-config :exit nil)
-    ("u" #'eyebrowse-close-window-config :exit nil)
+    ("k" #'eyebrowse-next-window-config :exit nil)
     ("i" #'eyebrowse-switch-to-window-config)
-    ("o" #'eyebrowse-rename-window-config :exit nil)
+    ("l" #'eyebrowse-last-window-config)
+    ("n" #'eyebrowse-rename-window-config :exit nil)
     ("q" nil)
     ("a" #'eyebrowse-switch-to-window-config-0)
     ("s" #'eyebrowse-switch-to-window-config-1)
@@ -3655,7 +3654,8 @@ current eyebrowse slot: %(eyebrowse--get 'current-slot)
     ("x" #'eyebrowse-switch-to-window-config-6)
     ("c" #'eyebrowse-switch-to-window-config-7)
     ("v" #'eyebrowse-switch-to-window-config-8)
-    ("b" #'eyebrowse-switch-to-window-config-9))
+    ("b" #'eyebrowse-switch-to-window-config-9)
+    ("~" #'eyebrowse-close-window-config :exit nil))
   (global-set-key (kbd "C-M-e") #'help/hydra-left-side/eyebrowse/body))
 ;; org_gcr_2017-07-08_mara_3D694F1B-EB4A-4724-A8E7-61F09C4773A5 ends here
 
