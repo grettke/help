cbae72bcaeef71a92ffa98d97adbab88b05c1e55
Grant Rettke <gcr@wisdomandwonder.com>
2017-08-06 10:47:04 -0500

    Programming language blog post helper

--- .emacs.el
+++ .emacs.el
@@ -764,6 +764,15 @@ Attribution: URL `https://rejeep.github.io/emacs/elisp/2010/11/16/delete-file-an
   (insert "#+CATEGORY: Article
 #+TAGS: Babel, Emacs, Ide, Lisp, Literate Programming, Programming Language, Reproducible research, elisp, org-mode
 #+TITLE:"))
+
+(defun help/wip ()
+  (interactive)
+  (when (use-region-p) (call-interactively 'kill-region))
+  (let ((name (read-string "Programming language name? ")))
+    (insert (format
+             "#+CATEGORY: Article
+#+TAGS: Programming Language, %s
+#+TITLE: (%s) " name name))))
 ;; org_gcr_2017-05-12_mara_7D37FFE5-2D2B-4CF7-AF27-F3CB8616D81B ends here
 
 ;; [[file:~/src/help/help.org::org_gcr_2017-05-12_mara_7354096C-3F3A-408E-8F1C-79ABB054040F][org_gcr_2017-05-12_mara_7354096C-3F3A-408E-8F1C-79ABB054040F]]
