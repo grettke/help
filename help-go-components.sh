74847212950f4fff5d4904908d5ef5d955f05a35
Grant Rettke <gcr@wisdomandwonder.com>
2017-08-25 23:29:37 -0500

    Add gore

--- help-go-components.sh
+++ help-go-components.sh
@@ -10,6 +10,7 @@ go get -u -v golang.org/x/tools/cmd/guru
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v github.com/jstemmer/gotags
 go get -u -v github.com/davecgh/go-spew/spew
+go get -u -v github.com/motemen/gore
 go get -u -v github.com/golang/example/hello
 go get -u -v github.com/tleyden/checkers-bot-minimax
 go get -u -v golang.org/x/tour/gotour
