4ba853da238b647506eceaf706ff72bdc28915b6
Grant Rettke <gcr@wisdomandwonder.com>
2017-08-25 23:24:46 -0500

    Verbose installs

--- help-go-components.sh
+++ help-go-components.sh
@@ -1,15 +1,15 @@
-go get -u github.com/rogpeppe/godef
-go get -u github.com/nsf/gocode
-go get -u golang.org/x/tools/cmd/godoc
-go get -u gopkg.in/alecthomas/gometalinter.v1
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/nsf/gocode
+go get -u -v golang.org/x/tools/cmd/godoc
+go get -u -v gopkg.in/alecthomas/gometalinter.v1
 gometalinter.v1 --install --update
-go get -u github.com/mdempsky/unconvert
-go get -u github.com/kisielk/errcheck
-go get -u honnef.co/go/tools/cmd/megacheck
-go get -u golang.org/x/tools/cmd/guru
-go get -u golang.org/x/tools/cmd/gorename
-go get -u github.com/jstemmer/gotags
-go get -u github.com/davecgh/go-spew/spew
-go get -u github.com/golang/example/hello
-go get -u github.com/tleyden/checkers-bot-minimax
-go get -u golang.org/x/tour/gotour
+go get -u -v github.com/mdempsky/unconvert
+go get -u -v github.com/kisielk/errcheck
+go get -u -v honnef.co/go/tools/cmd/megacheck
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v github.com/jstemmer/gotags
+go get -u -v github.com/davecgh/go-spew/spew
+go get -u -v github.com/golang/example/hello
+go get -u -v github.com/tleyden/checkers-bot-minimax
+go get -u -v golang.org/x/tour/gotour
