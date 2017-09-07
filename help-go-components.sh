f1705254719cc794e8048e0e29dca96d4abdb0db
Grant Rettke <gcr@wisdomandwonder.com>
2017-09-07 10:05:35 -0400

    Simply go package install

--- help-go-components.sh
+++ help-go-components.sh
@@ -1,11 +1,8 @@
 go get -u -v github.com/rogpeppe/godef
 go get -u -v github.com/nsf/gocode
 go get -u -v golang.org/x/tools/cmd/godoc
-go get -u -v gopkg.in/alecthomas/gometalinter.v1
-gometalinter.v1 --install --update
-go get -u -v github.com/mdempsky/unconvert
-go get -u -v github.com/kisielk/errcheck
-go get -u -v honnef.co/go/tools/cmd/megacheck
+go get -u -v github.com/alecthomas/gometalinter
+gometalinter --install
 go get -u -v golang.org/x/tools/cmd/guru
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v github.com/jstemmer/gotags
