fdfbe5658023ef179662e3296fbe2a1bb464ddfe
Grant Rettke <gcr@wisdomandwonder.com>
2018-05-25 14:38:29 -0400

    (nhncs) Adds shell keybinding.

--- .emacs.el
+++ .emacs.el
@@ -4542,6 +4542,10 @@ _m_ Disable Unhelpful Modes _M_ Enable Unhelpful Modes
                 (visual-line-mode nil)))))
 ;; org_gcr_2017-05-12_mara_D0F37886-4355-478C-A141-93F48E531CE5 ends here
 
+;; [[file:~/src/help/help.org::org_gcr_2018-05-24T12-09-16-04-00_mara_09A8135C-9B9B-4039-A091-6DB640A7C3EF][org_gcr_2018-05-24T12-09-16-04-00_mara_09A8135C-9B9B-4039-A091-6DB640A7C3EF]]
+(global-set-key (kbd "M-s M-s") #'shell)
+;; org_gcr_2018-05-24T12-09-16-04-00_mara_09A8135C-9B9B-4039-A091-6DB640A7C3EF ends here
+
 ;; [[file:~/src/help/help.org::org_gcr_2017-05-12_mara_63E4B554-A5C6-46AB-9A34-E93FAF8B848B][org_gcr_2017-05-12_mara_63E4B554-A5C6-46AB-9A34-E93FAF8B848B]]
 ;; Leave C-z free for Tmux
 (global-set-key (kbd "C-z") nil)
