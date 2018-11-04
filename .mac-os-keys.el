2b36f2c8dbf36a2245eff97e19521041a04bb398
Grant Rettke <gcr@wisdomandwonder.com>
2018-11-04 03:35:29 -0600

    (rfns) new keyboard mapping

--- .mac-os-keys.el
+++ .mac-os-keys.el
@@ -4,6 +4,7 @@
 (setq mac-right-command-modifier 'left)
 (setq mac-option-modifier 'super)
 (setq mac-right-option-modifier 'left)
+(setq mac-function-modifier 'hyper)
 (defun help/toggle-mac-right-option-modifier ()
   "Toggle between passing option modifier either to Emacs or macOS."
   (interactive)
@@ -15,14 +16,3 @@
     (message "Toggled `mac-right-option-modifier' from %s to %s."
              old-ropt
              mac-right-option-modifier)))
-(defun help/toggle-mac-function-modifier ()
-  "Toggle between passing function modifier either to Emacs or macOS."
-  (interactive)
-  (let ((old-func mac-function-modifier))
-    (setq mac-function-modifier
-          (if (eq mac-function-modifier 'hyper)
-              'none
-            'hyper))
-    (message "Toggled `mac-function-modifier' from %s to %s."
-             old-func
-             mac-function-modifier)))
