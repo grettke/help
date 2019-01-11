1bae7fd78346299ea05ad0962a42b0f2d8aacaac
Grant Rettke <gcr@wisdomandwonder.com>
2019-01-10 22:45:29 -0600

    (nhncs) Filesets

--- custom.el
+++ custom.el
@@ -17,7 +17,12 @@
    (quote
     ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d847e7d718be4e70404aaacd1f293ea97342882f665d0c8e0eb9065cb3393625" "c2471caadabc64850540a344079c07e4c219cd6c9a48811584438e0a0d6dc3e2" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(fci-rule-color "#073642")
- '(filesets-data (quote (("HELP" (:pattern "~/src/help" "^.+\\.org$")))))
+ '(filesets-data
+   (quote
+    (("HELP"
+      (:pattern "~/src/help" "^.+\\.org$"))
+     ("Poetry"
+      (:pattern "~/src/poetry" "^.+\\.org$")))))
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
    (--map
