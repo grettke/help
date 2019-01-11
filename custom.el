682b34c0bc32c14e3a68f3994c463424806c5e36
Grant Rettke <gcr@wisdomandwonder.com>
2019-01-11 00:02:58 -0600

    (nhncs) Fileset

--- custom.el
+++ custom.el
@@ -19,7 +19,9 @@
  '(fci-rule-color "#073642")
  '(filesets-data
    (quote
-    (("Org2Blog"
+    (("WIP"
+      (:files "~/src/list/scratch.org" "~/src/list/projects.org"))
+     ("Org2Blog"
       (:files "~/src/xml-rpc-el/xml-rpc.el" "~/src/metaweblog/metaweblog.el" "~/src/org2blog/ox-wp.el" "~/src/org2blog/org2blog.el"))
      ("HELP"
       (:pattern "~/src/help" "^.+\\.\\(org\\|el\\)$"))
