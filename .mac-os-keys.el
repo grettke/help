67d3715c207466b82dbf5d441c8d18577cf7a47f
Grant Rettke <gcr@wisdomandwonder.com>
2019-05-18 19:35:12 -0500

    (nhncs) durgod keys

--- .mac-os-keys.el
+++ .mac-os-keys.el
@@ -1,12 +1,23 @@
+;; | Key                            | My Emacs | OS Note                           |
+;; |--------------------------------+----------+-----------------------------------|
+;; | lcl : Left-Control             | Control  | Rarely used                       |
+;; | lgi : Left-GUI (Command, Win)  | Meta     | Mac Window Switching              |
+;; | lal : Left-Alt (Option)        | Super    | Mac symbols, Win Window Switching |
+;; | rcl : Right-Control            | Hyper    | Rarely used                       |
+;; | rgi : Right-GUI (Command, Win) | Alt      | Mac Window Switching              |
+;; | ral : Right-Alt (Option)       | Alt∨None | Mac symbols, Win Window Switching |
+
+(setq ns-control-modifier 'control)
 (setq ns-command-modifier 'meta)
 (setq ns-alternate-modifier 'super)
-(setq ns-right-alternate-modifier 'ignore)
 (setq ns-right-control-modifier 'hyper)
+(setq ns-right-command-modifier 'alt)
+(setq ns-right-alternate-modifier 'alt)
+
 (defun help/toggle-ns-right-alternate-modifier ()
-  "Tottle right alternate key between meta or ignored by Emacs and passed
-though to the operating system."
+  "Either Emacs swallows right-alt as alt or passes it to OS."
   (interactive)
   (setq ns-right-alternate-modifier
-        (cond ((eq ns-right-alternate-modifier 'super) 'none)
-              ((eq ns-right-alternate-modifier 'none) 'super)
-              (t 'super))))
+        (cond ((eq ns-right-alternate-modifier 'alt) 'none)
+              ((eq ns-right-alternate-modifier 'none) 'alt)
+              (t 'alt))))
