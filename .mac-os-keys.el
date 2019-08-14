bc42f6471c14a7d86e16682e26e5d309446821fb
Grant Rettke <gcr@wisdomandwonder.com>
2019-08-14 12:19:47 -0500

    New mac layout

--- .mac-os-keys.el
+++ .mac-os-keys.el
@@ -1,23 +1,11 @@
-;; | Key                            | My Emacs | OS Note                           |
-;; |--------------------------------+----------+-----------------------------------|
-;; | lcl : Left-Control             | Control  | Rarely used                       |
-;; | lgi : Left-GUI (Command, Win)  | Meta     | Mac Window Switching              |
-;; | lal : Left-Alt (Option)        | Super    | Mac symbols, Win Window Switching |
-;; | rcl : Right-Control            | Hyper    | Rarely used                       |
-;; | rgi : Right-GUI (Command, Win) | Alt      | Mac Window Switching              |
-;; | ral : Right-Alt (Option)       | Alt∨None | Mac symbols, Win Window Switching |
-
 (setq ns-control-modifier 'control)
-(setq ns-command-modifier 'meta)
-(setq ns-alternate-modifier 'super)
-(setq ns-right-control-modifier 'hyper)
-(setq ns-right-command-modifier 'alt)
-(setq ns-right-alternate-modifier 'alt)
 
-(defun help/toggle-ns-right-alternate-modifier ()
-  "Either Emacs swallows right-alt as alt or passes it to OS."
-  (interactive)
-  (setq ns-right-alternate-modifier
-        (cond ((eq ns-right-alternate-modifier 'alt) 'none)
-              ((eq ns-right-alternate-modifier 'none) 'alt)
-              (t 'alt))))
+(setq ns-command-modifier 'meta)
+
+(setq ns-alternate-modifier 'hyper)
+
+(setq ns-right-control-modifier 'super)
+
+(setq ns-right-command-modifier 'alt)
+
+(setq ns-right-alternate-modifier 'hyper)
